c4d0f66a6bb74d990c041dac1be67d73f7efeb31
richpingree <richpingree@fullsail.edu>
2014-01-22 22:44:37 -0600

    recoded data due to having issues with original list
    switched to a regular list instead of thumbnail list due to could not
    get event listeners to work with images.

--- Pingree_Richard_Prototype1/Resources/data.js
+++ Pingree_Richard_Prototype1/Resources/data.js
@@ -1,19 +1,22 @@
 var stock = {
-	"inventory": {
-		headTitle: "",
-		footTitle: "",
+	"inventory":{
+		headTitle: "Video Games",
 		"games": [
 			{"title": "Lego Batman", 
 			"esrbRating": "E 10+ - Everyone 10 & up",
 			"esrbDesc": "Cartoon Violence",
 			"genre": "Action-Adventure",
 			"developer": "Traveller's Tale/TT Fusion",
-			"publisher": " Warner Bros. Interactive Entertainment",
+			"publisher": "Warner Bros. Interactive Entertainment",
 			"system": "Cloud(OnLive), Mac OS X, Microsoft Windows, Mobile Phones, Nintendo DS, Playstation 2, Playstation 3, PSP, Wii, Xbox 360",
 			"modes": "Single and Co-op",
 			"co-op": "Yes",
 			"players": "1-2",
-			"photo": "photo/Lego_Batman.jpg"},
+			"rate": "4.0/5.0",
+			"user": "david1245",
+			"date": "1/13/2012",
+			"review": "Best lego hero ever. Great game to spend time with a close friend or family member",
+			"photo": "photos/Lego_Batman.jpg"},
 			
 			{"title": "Mortal Kombat vs. DC Universe", 
 			"esrbRating": "T - Teen",
@@ -25,7 +28,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "N/A",
 			"players": "1-2",
-			"photo": "photo/20Mortal_Kombat_vs._DC_Universe_Coverart.png"},
+			"rate": "4.0/5.0",
+			"user": "jason85",
+			"date": "1/13/2012",
+			"review": "Great game. Never thought these two worlds would ever collide",
+			"photo": "photos/20Mortal_Kombat_vs._DC_Universe_Coverart.png"},
 			
 			{"title": "The Elder Scroll V: Skyrim", 
 			"esrbrating": "M -Mature",
@@ -36,8 +43,11 @@ var stock = {
 			"system": "Microsoft Windows, Playstation 3, Xbox 360",
 			"modes": "Single",
 			"co-op": "N/A",
-			"players": "1",
-			"photo": "photo/Skyrim.jpg"},
+			"players": "1","rate": "4.0/5.0",
+			"user": "josh85",
+			"date": "1/13/2012",
+			"review": "One of the greatest RPG's ever made, in my option. Just roaming around the open world is worth the purchase",
+			"photo": "photos/Skyrim.jpg"},
 			
 			{"title": "Bully", 
 			"esrbrating": "T - Teen",
@@ -49,7 +59,11 @@ var stock = {
 			"modes": "Single",
 			"co-op": "No",
 			"players": "1",
-			"photo": "photo/Bully.jpg"},
+			"rate": "4.0/5.0",
+			"user": "theone78",
+			"date": "1/13/2012",
+			"review": "Wish I could of got away with some of these thing in school.",
+			"photo": "photos/Bully.jpg"},
 				
 			{"title": "Left 4 Dead", 
 			"esrbRating": "M - Mature",
@@ -61,7 +75,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "Yes",
 			"players": "1-2, Multiplayer: 2-8",
-			"photo": "photo/Left_4_Dead.jpg"},
+			"rate": "4.0/5.0",
+			"user": "alex2010",
+			"date": "1/13/2012",
+			"review": "Great game. Love the multiplayer mode. Co-op mode is also enjoyable.",
+			"photo": "photos/Left_4_Dead.jpg"},
 			
 			{"title": "Dead Rising 2", 
 			"esrbRating": "M - Mature",
@@ -73,7 +91,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "Yes(Online Only)",
 			"players": "1-2, Multiplayer: 2-4",
-			"photo": "photo/550w_gaming_deadrising2_box.jpg"},
+			"rate": "4.0/5.0",
+			"user": "steven2013",
+			"date": "1/13/2012",
+			"review": "Good for a quick play or a full day of playing. The weapon selection is amazing.",
+			"photo": "photos/550w_gaming_deadrising2_box.jpg"},
 			
 			{"title": "Tiger Woods PGA Tour 14", 
 			"esrbRating": "E - Everyone",
@@ -85,7 +107,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "Yes",
 			"players": "1-4",
-			"photo": "photo/tiger_woods_pga_tour_14.jpg"},
+			"rate": "4.0/5.0",
+			"user": "tiger13",
+			"date": "1/13/2012",
+			"review": "Great game. Enjoy play all over the world without having to leave my living room.",
+			"photo": "photos/tiger_woods_pga_tour_14.jpg"},
 			
 			{"title": "Tom Clancy's: Splinter Cell Blacklist", 
 			"esrbRating": "M - Mature",
@@ -97,7 +123,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "Yes",
 			"players": "1-2, Multiplayer: 2-8",
-			"photo": "photo/Splinter_Cell_Blacklist.jpg"},
+			"rate": "4.0/5.0",
+			"user": "kenny12",
+			"date": "1/13/2012",
+			"review": "Best stealth game I've every played.",
+			"photo": "photos/Splinter_Cell_Blacklist.jpg"},
 			
 			{"title": "Lego Marvel Super Heroes", 
 			"esrbRating": "E 10+ - Everone 10 & up",
@@ -109,7 +139,11 @@ var stock = {
 			"modes": "Single and Multiplayer",
 			"co-op": "Yes",
 			"players": "1-2",
-			"photo": "photo/LEGO_Marvel_Super_Heroes_box_art.jpg"},
+			"rate": "4.0/5.0",
+			"user": "winston@43",
+			"date": "1/13/2012",
+			"review": "Amazing due to the fact of all the playable characters.",
+			"photo": "photos/LEGO_Marvel_Super_Heroes_box_art.jpg"},
 			
 			{"title": "Little Big Planet 2", 
 			"esrbRating": "E - Everyone",
@@ -119,29 +153,189 @@ var stock = {
 			"publisher": "Sony Computer Entertainment",
 			"system": "Playstation 3",
 			"modes": "Single and Multiplayer",
-			"co-op": "Yes",
+			"coop": "Yes",
 			"players": "1-4",
-			"photo": "photo/little_big_planet_2_boxart.jpg"},
-		]		
-	}
+			"rate": "4.0/5.0",
+			"user": "josh 85",
+			"date": "1/13/2012",
+			"review": "Great game. Love the multiplayer mode.",
+			"photo": "photos/little_big_planet_2_boxart.jpg"}
+		]
+	}			
 };
 
-// for(n in stock){
-	// for(var i = 0; i<stock[n].games.length; i++){
-		// var listview = Ti.UI.createView({
-			// title: stock[n].games[i].title,
-			// esrb: stock[n].games[i].esrbRating,
-			// desc: stock[n].games[i].esrbDesc,
-			// genre: stock[n].games[i].genre,
-			// dev: stock[n].games[i].developer,
-			// pub: stock[n].games[i].publisher,
-			// sys: stock[n].games[i].system,
-			// modes: stock[n].games[i].modes,
-			// coop: stock[n].games[i].co-op,
-			// play: stock[n].games[i].players,
-			// photo: stock[n].games[i].photo,
-			// hasChild: true			
-		// });
-	// };
-// 	
-// };
+var table = Ti.UI.createTableView({
+	style: Ti.UI.iPhone.TableViewStyle.GROUPED
+	
+});
+
+var sections = [];
+for(n in stock){
+	var tableSecs = Ti.UI.createTableViewSection({
+		headerTitle: stock[n].headTitle
+	});
+	for(var i = 0; i<stock[n].games.length; i++){
+		var theRow = Ti.UI.createTableViewRow({
+			title: stock[n].games[i].title,
+			esrb: stock[n].games[i].esrbRating,
+			desc: stock[n].games[i].esrbDesc,
+			genre: stock[n].games[i].genre,
+			dev: stock[n].games[i].developer,
+			pub: stock[n].games[i].publisher,
+			sys: stock[n].games[i].system,
+			modes: stock[n].games[i].modes,
+			coop: stock[n].games[i].coop,
+			play: stock[n].games[i].players,
+			rate: stock[n].games[i].rate,
+			user: stock[n].games[i].user,
+			date: stock[n].games[i].user,
+			review: stock[n].games[i].review,
+			photo: stock[n].games[i].photo,
+			hasChild: true
+		});
+		tableSecs.add(theRow);
+
+	};
+	sections.push(tableSecs);
+};
+
+loginButton.addEventListener("click", function(e){
+	var dataWin = Ti.UI.createWindow({
+		title: "Video Games",
+		modal: true,
+		backgroundColor: "#595BC2"
+	});
+		dataWin.add(table);
+		navWin.openWindow(dataWin, {animate: true});
+});
+
+table.addEventListener("click", function(event){
+	var detailWin = Ti.UI.createWindow({
+		backgroundColor: "#595BC2",
+		title: event.source.title,
+		modal: true
+	});
+	var detailView = Ti.UI.createScrollView({
+		showVerticalScaleIndicator: true,
+		layout: "vertical"
+		
+	});
+	var logoImage = Ti.UI.createImageView({
+		image: event.source.photo,
+		borderRadius: 20,
+		height: 200,
+		top: 15,
+		left: 20,
+		right: 20
+		
+	});
+	var titleText = Ti.UI.createLabel({
+		text: "Title: " + event.source.title,
+		color: "#fff",
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var esrbText = Ti.UI.createLabel({
+		text: "ESRB Rating: " + event.source.esrb,
+		color: "#fff",
+		top: (titleText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var esrbDescText = Ti.UI.createLabel({
+		text: "ESRB Description: " + event.source.desc,
+		color: "#fff",
+		top: (esrbText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var genreText = Ti.UI.createLabel({
+		text: "Genre: " + event.source.genre,
+		color: "#fff",
+		top: (esrbDescText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var devText = Ti.UI.createLabel({
+		text: "Developer: " + event.source.dev,
+		color: "#fff",
+		top: (genreText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var pubText = Ti.UI.createLabel({
+		text: "Publisher: " + event.source.pub,
+		color: "#fff",
+		top: (devText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var sysText = Ti.UI.createLabel({
+		text: "System(s): " + event.source.sys,
+		color: "#fff",
+		top: (pubText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var modeText = Ti.UI.createLabel({
+		text: "Game Modes: " + event.source.modes,
+		color: "#fff",
+		top: (sysText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var coopText = Ti.UI.createLabel({
+		text: "Co-op: " + event.source.coop,
+		color: "#fff",
+		top: (modeText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var playersText = Ti.UI.createLabel({
+		text: "Number of Players: " + event.source.play,
+		color: "#fff",
+		top: (coopText.top + 15),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var reviewLabel = Ti.UI.createLabel({
+		text: "Reviews:",
+		color: "#fff",
+		top: (playersText.top + 20),
+		left: 10,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var rateLabel = Ti.UI.createLabel({
+		text: "Rating: " + event.source.rate,
+		color: "#fff",
+		top: (reviewLabel.top + 20),
+		left: 15,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var userLabel = Ti.UI.createLabel({
+		text: "User Name: " + event.source.user,
+		color: "#fff",
+		top: (rateLabel.top + 20),
+		left: 15,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var dateLabel = Ti.UI.createLabel({
+		text: "Review Date: " + event.source.date,
+		color: "#fff",
+		top: (userLabel.top + 20),
+		left: 15,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	var userReviewLabel = Ti.UI.createLabel({
+		text: "Review: " + event.source.review,
+		color: "#fff",
+		top: (dateLabel.top + 20),
+		left: 15,
+		font: {fontSize: 15, fontFamily: "Times New Roman"}
+	});
+	detailView.add(logoImage, titleText, esrbText, esrbDescText, genreText, devText, pubText, sysText, modeText, coopText, playersText, reviewLabel, rateLabel, userLabel, dateLabel, userReviewLabel);
+	detailWin.add(detailView);
+	navWin.openWindow(detailWin, {animate: true});
+	
+});
+table.setData(sections);
